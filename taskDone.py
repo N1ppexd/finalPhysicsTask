617f76f48294c9f9dec8fd4475ee7d3aa687d7bb
N1ppexd <niilohanni@gmail.com>
2026-01-04 21:39:40 +0200

    Done

--- taskDone.py
+++ taskDone.py
@@ -15,6 +15,12 @@ location_data = pd.read_csv(locationUrl)
 from scipy.signal import butter, filtfilt
 
 
+
+st.title('Fysikan lopputyö')
+st.write('Tässä näkyy askelmäärät, keskinopeus, tehospektri ja reitti kartalla.')
+
+
+
 def butter_lowpass_filter(data, cutoff, fs, nyq, order):
     normal_cutoff = cutoff / nyq
     b, a = butter(order, normal_cutoff, btype='low', analog=False)
@@ -30,7 +36,7 @@ def butter_highpass_filter(data, cutoff, fs, nyq, order):
 #T is the length of the signal
 #last element minus first element means total time duration
 t = acceleration_data['Time (s)'].iloc[-1] - acceleration_data['Time (s)'].iloc[0]
-st.write(f'Time duration of the signal: {t} seconds')
+st.write(f'Signaalin kesto: {t} sekuntia.')
 
 fs = len(acceleration_data['Time (s)']) / t  # Sampling frequency
 nyq = 0.5 * fs  # Nyquist Frequency
@@ -39,9 +45,15 @@ cutoff_low = 2.5  # Desired cutoff frequency of the filter, Hz
 
 filtered_z_low = butter_lowpass_filter(acceleration_data['Linear Acceleration z (m/s^2)'], cutoff_low, fs, nyq, order)
 
+fig, ax = plt.subplots(figsize=(24, 6))
+ax.plot(acceleration_data['Time (s)'], acceleration_data['Linear Acceleration z (m/s^2)'], alpha=0.7)
+ax.plot(acceleration_data['Time (s)'], filtered_z_low, label='Lowpass Filtered Z-axis')
+ax.legend(('Alkuperäinen Z-akseli','Alipäästösuodatettu Z-akseli'))
+ax.set_ylabel('Acceleration (m/s²)')
+ax.set_title('Alipäästösuodatettu Z-akseli verrattuna alkuperäiseen signaaliin')
+ax.grid()
+st.pyplot(fig)
 
-st.line_chart(acceleration_data['Linear Acceleration z (m/s^2)'])
-st.line_chart(filtered_z_low)
 
 stepcount = 0
 
@@ -50,7 +62,7 @@ for i in range(1, len(filtered_z_low) - 1):
     if((filtered_z_low[i] > 0 and filtered_z_low[i] > filtered_z_low[i - 1] and filtered_z_low[i] > filtered_z_low[i + 1])):
         stepcount += 1
 
-st.write(f"Total steps counted: {stepcount}.")
+st.write(f"Askelten määrä laskettuna alipäästösuodatetusta signaalista: {stepcount}.")
 
 
 #calculate step count from fourier transformed signal
@@ -60,20 +72,20 @@ N = len(acceleration_data['Linear Acceleration z (m/s^2)'])
 yf = fft(acceleration_data['Linear Acceleration z (m/s^2)'])
 xf = fftfreq(N, 1 / fs)
 
-st.line_chart({'Frequency (Hz)': xf[:N // 2],
-'Amplitude': 2.0 / N * np.abs(yf[:N // 2])})
-
-
+fig, ax = plt.subplots()
+ax.plot(xf[:N // 2], 2.0 / N * np.abs(yf[:N // 2]))
+st.write("Tehospektri:")
+st.pyplot(fig)
 
 #find the peak frequency in the fourier transformed signal
 amplitudes = 2.0 / N * np.abs(yf[:N // 2])
 peak_freq_index = np.argmax(amplitudes)
 peak_frequency = xf[peak_freq_index]
-print(f"Peak frequency in the Fourier Transform: {peak_frequency} Hz")
+st.write(f"Korkein taajuus: {peak_frequency} Hz.")
 
 #calculate step count from peak frequency
 calculated_steps_fourier = peak_frequency * t
-print(f"Calculated steps: {calculated_steps_fourier}.")
+st.write(f"Tehospektrillä laskettu askelmäärä: {calculated_steps_fourier}.")
 
 
 
@@ -96,12 +108,12 @@ for i in range(1, len(location_data['Latitude (°)'])):
     lon2 = location_data['Longitude (°)'][i]
     distance += haversine(lat1, lon1, lat2, lon2)
 
-st.write(f'Total distance traveled (in meters): {distance * 1000} meters')
+st.write(f'Matka metreinä: {distance * 1000} metriä')
 
 
 #let's calculate average speed
 average_speed = (distance * 1000) / t  # in meters per second
-st.write(f'Average speed: {average_speed} m/s')
+st.write(f'Keskinopeus: {average_speed} m/s')
 
 
 
@@ -118,6 +130,7 @@ for i in range(len(location_data['Latitude (°)'])):
     ).add_to(map)
 
 folium.PolyLine(location_data[['Latitude (°)', 'Longitude (°)']], color='red').add_to(map)
-map.save('location_map.html')
+
+st.write("Reitti kartalla:")
 
 st_map = st_folium(map, width=700, height=500)
